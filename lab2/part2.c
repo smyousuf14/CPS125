aff4eae39aa734c94e8beb7421916c83f084ed32
smyousuf14 <44485548+smyousuf14@users.noreply.github.com>
2019-07-03 12:27:41 -0400

    Create part2.c

---
+++ lab2/part2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    // Local Variable
+    double number;
+    
+    scanf("%lf", &number);
+    
+    printf("Nearest hundredth: %.2lf", number);
+
+    return 0;
+}
